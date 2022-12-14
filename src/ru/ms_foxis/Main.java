637398f78b90c9c09d92c963cae4f21f66abfea8
msfoxis <kristina_kuznetsova_1999@mail.ru>
2022-12-14 21:26:30 +0600

    comment1

--- src/ru/ms_foxis/Main.java
+++ src/ru/ms_foxis/Main.java
@@ -3,6 +3,6 @@ package ru.ms_foxis;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, everyone!");
-        System.out.println("I'm new here!");
+        System.out.println("Conflict!");
     }
 }
